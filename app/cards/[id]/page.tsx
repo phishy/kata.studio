a226baa15493a0fc4dd1f8f5da31f2ca17626fd3
Jeff Loiselle <jeff@loiselles.com>
2023-09-21 14:36:58 -0500

    adds card page metadata

--- app/cards/[id]/page.tsx
+++ app/cards/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata, ResolvingMetadata } from "next"
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 
@@ -11,6 +12,28 @@ interface PageProps {
   }
 }
 
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const supabase = createServerComponentClient({ cookies })
+  let { error, data: card } = await supabase
+    .from("cards")
+    .select("*")
+    .eq("id", params.id)
+    .single()
+
+  return {
+    title: `${card.title} - kata.studio`,
+    description: `${card.question}`,
+  }
+}
+
 export default async function CardForm({ params }: PageProps) {
   const supabase = createServerComponentClient({ cookies })
   let { error, data } = await supabase
@@ -19,6 +42,5 @@ export default async function CardForm({ params }: PageProps) {
     .eq("id", params.id)
     .single()
 
-    return <Answer card={data} />
-
+  return <Answer card={data} />
 }
