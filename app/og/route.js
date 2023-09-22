2c40171faf36d8d668bf38b442f0a4bb35189f10
Jeff Loiselle <jeff@loiselles.com>
2023-09-21 22:32:49 -0500

    temp revert dynamic cards

--- app/og/route.js
+++ app/og/route.js
@@ -7,19 +7,20 @@ import { cookies } from 'next/headers'
 export const runtime = "edge"
 
 export async function GET(request) {
-  const { searchParams } = new URL(request.url)
-  const id = searchParams.get('id')
+  // const { searchParams } = new URL(request.url)
+  // const id = searchParams.get('id')
 
-  const supabase = createRouteHandlerClient({ cookies })
-  const { data: card } = await supabase.from('cards').select().eq('id', id).single()
+  // const supabase = createRouteHandlerClient({ cookies })
+  // const { data: card } = await supabase.from('cards').select().eq('id', id).single()
   return new ImageResponse(
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
         <div tw="bg-gray-50 flex w-full">
           <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
             <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
-              <span>{card.title}</span>
-              <span tw="text-indigo-600">kata.studio</span>
+              {/* <span>{card.title}</span> */}
+              <span>What is a variable?</span>
+              <span tw="text-indigo-600">Learn JavaScript @ kata.studio</span>
             </h2>
             <div tw="mt-8 flex md:mt-0">
               <div tw="flex rounded-md shadow">
