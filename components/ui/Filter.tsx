bb33f503204d4590c55b9ea7ee4d4d59c983a062
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2023-09-29 09:33:52 -0400

    filter dropdown closes if user clicks outside of it

--- components/ui/Filter.tsx
+++ components/ui/Filter.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 export default function Filter() {
   const router = useRouter();
+  const dropdownRef = useRef(null);
   const [isOpen, setIsOpen] = React.useState(false);
 
   const handleFilterClick = (difficulty) => {
@@ -18,8 +19,21 @@ export default function Filter() {
     router.push('/cards');
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target) && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <div className="relative">
+    <div className="relative" ref={dropdownRef}>
       <button className="flex-none rounded-md bg-purple-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-500" onClick={handleFilterOpen}>
         Filters
       </button>
