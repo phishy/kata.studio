e94d0af3a3b8436d55358168ad63a13333e864d3
Chelsey Gowac <97612806+chelsey-g@users.noreply.github.com>
2023-10-02 13:08:35 -0400

    added multi select feature for difficulty levels (#8)

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -34,7 +34,7 @@ export default async function Page({ params, searchParams }: PageProps) {
   } 
   
   if (searchParams.difficulty) {
-    query.eq("difficulty", searchParams.difficulty)
+    query.in("difficulty", [searchParams.difficulty])
 
   }
 
